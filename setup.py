3ff88a78d3004341630258f2b51214e95cec603a
Tian <2970701140@qq.com>
2025-07-28 22:40:29 +0800

    change readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="faststt",
     version="0.1.0",
-    description="基于 faster-whisper 的实时语音识别系统",
+    description="基于 faster-whisper 的语音识别系统",
     author="p11grim",
     author_email="2970701140@qq.com",
     url="https://github.com/p11grim/faststt",
