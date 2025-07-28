e13d3406338204190cb8bd8b1e8bdfc634e93d59
Tian <2970701140@qq.com>
2025-07-28 22:06:44 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="faststt",
+    version="0.1.0",
+    description="基于 faster-whisper 的实时语音识别系统",
+    author="p11grim",
+    author_email="2970701140@qq.com",
+    url="https://github.com/p11grim/faststt",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    include_package_data=True,
+)
